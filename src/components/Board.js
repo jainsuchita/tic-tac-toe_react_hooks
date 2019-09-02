14b4d0351af94c5a26825d816d7b3fa9b74a4d67
jainsuchi21 <jainsuchi21@gmail.com>
2019-09-02 22:43:56 +0530

    Add useEffect hook

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,71 +1,71 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import {
-    SQUARE_DIMS, DIMS, PLAYER_O, PLAYER_X,
-    getRandomInt,
-} from "utils";
+// import React, { useState } from "react";
+// import styled from "styled-components";
+// import {
+//     SQUARE_DIMS, DIMS, PLAYER_O, PLAYER_X,
+//     getRandomInt,
+// } from "utils";
 
-import Square from "./Square";
+// import Square from "./Square";
 
-const arr = new Array(DIMS ** 2).fill(null);
+// const arr = new Array(DIMS ** 2).fill(null);
 
-const Board = ({ players }) => {
-    const [grid, setGrid] = useState(arr);
-    // const [players, setPlayers] = useState({
-    //     human: PLAYER_X,
-    //     computer: PLAYER_O
-    // });
+// const Board = ({ players }) => {
+//     const [grid, setGrid] = useState(arr);
+//     // const [players, setPlayers] = useState({
+//     //     human: PLAYER_X,
+//     //     computer: PLAYER_O
+//     // });
 
-    const move = (index, player) => {
-        setGrid(grid => {
-            const gridCopy = grid.concat();
-            gridCopy[index] = player;
+//     const move = (index, player) => {
+//         setGrid(grid => {
+//             const gridCopy = grid.concat();
+//             gridCopy[index] = player;
 
-            console.log(gridCopy)
-            return gridCopy;
-        });
-    };
+//             console.log(gridCopy)
+//             return gridCopy;
+//         });
+//     };
 
-    const computerMove = () => {
-        let index = getRandomInt(0, 8);
-        while (grid[index]) {
-            index = getRandomInt(0, 8);
-        }
-        move(index, players.computer);
-    };
+//     const computerMove = () => {
+//         let index = getRandomInt(0, 8);
+//         while (grid[index]) {
+//             index = getRandomInt(0, 8);
+//         }
+//         move(index, players.computer);
+//     };
 
-    const humanMove = index => {
-        if (!grid[index]) {
-            move(index, players.human);
-            computerMove();
-        }
-    };
+//     const humanMove = index => {
+//         if (!grid[index]) {
+//             move(index, players.human);
+//             setNextMove(players.computer);
+//         }
+//     };
 
-    return (
-        <Container dims={DIMS}>
-            {
-                grid.map((value, index) => {
-                    const isActive = value !== null;
-                    return (
-                        <Square
-                            isActive={isActive}
-                            value={value === PLAYER_X ? "X" : "O"}
-                            key={index}
-                            onClick={() => humanMove(index)}
-                        />
-                    );
-                })
-            }
-        </Container>
-    );
-}
+//     return (
+//         <Container dims={DIMS}>
+//             {
+//                 grid.map((value, index) => {
+//                     const isActive = value !== null;
+//                     return (
+//                         <Square
+//                             isActive={isActive}
+//                             value={value === PLAYER_X ? "X" : "O"}
+//                             key={index}
+//                             onClick={() => humanMove(index)}
+//                         />
+//                     );
+//                 })
+//             }
+//         </Container>
+//     );
+// }
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  width: ${({ dims }) => `${dims * (SQUARE_DIMS + 5)}px`};
-  flex-flow: wrap;
-  position: relative;
-`;
+// const Container = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   width: ${({ dims }) => `${dims * (SQUARE_DIMS + 5)}px`};
+//   flex-flow: wrap;
+//   position: relative;
+// `;
 
-export default Board;
+// export default Board;
--- src/tictactoe.js
+++ src/tictactoe.js
@@ -1,19 +1,80 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import styled from "styled-components";
-import { GAME_STATES, PLAYER_X, PLAYER_O, switchPlayer } from "utils";
-import { Board } from "components";
+import {
+    GAME_STATES, PLAYER_X, PLAYER_O, switchPlayer,
+    SQUARE_DIMS, DIMS,
+    getRandomInt,
+} from "utils";
+import { Square } from "components";
+
+const arr = new Array(DIMS ** 2).fill(null);
 
 const Tictactoe = () => {
+    const [grid, setGrid] = useState(arr);
+
     const [gameState, setGameState] = useState(GAME_STATES.notStarted);
+
     const [players, setPlayers] = useState({
         human: PLAYER_X,
         computer: PLAYER_O
     });
 
+    const [nextMove, setNextMove] = useState(null);
+
+    const move = useCallback(
+        (index, player) => {
+            if (player && gameState === GAME_STATES.inProgress) {
+                setGrid(grid => {
+                    const gridCopy = grid.concat();
+                    gridCopy[index] = player;
+                    return gridCopy;
+                });
+            }
+        },
+        [gameState]
+    );
+
+    const computerMove = useCallback(() => {
+        let index = getRandomInt(0, 8);
+        while (grid[index]) {
+            index = getRandomInt(0, 8);
+        }
+
+        move(index, players.computer);
+        setNextMove(players.human);
+
+    }, [move, grid, players]);
+
+    const humanMove = index => {
+        if (!grid[index]) {
+            move(index, players.human);
+            setNextMove(players.computer);
+        }
+    };
+
     const choosePlayer = option => {
         setPlayers({ human: option, computer: switchPlayer(option) });
         setGameState(GAME_STATES.inProgress);
+        setNextMove(PLAYER_X); // Set the Player X to make the first move
     };
+
+    useEffect(() => {
+        let timeout;
+        if (
+            nextMove !== null &&
+            nextMove === players.computer &&
+            gameState !== GAME_STATES.over
+        ) {
+            // Delay computer moves to make them more natural
+            timeout = setTimeout(() => {
+                computerMove();
+            }, 500);
+        }
+
+        // ComponentUnmount lifecycle hook
+        return () => timeout && clearTimeout(timeout);
+
+    }, [nextMove, computerMove, players.computer, gameState]);
 
     if (gameState === GAME_STATES.notStarted) {
         return (
@@ -32,7 +93,21 @@ const Tictactoe = () => {
 
     else {
         return (
-            <Board players={players} />
+            <Container dims={DIMS}>
+                {
+                    grid.map((value, index) => {
+                        const isActive = value !== null;
+                        return (
+                            <Square
+                                isActive={isActive}
+                                value={value === PLAYER_X ? "X" : "O"}
+                                key={index}
+                                onClick={() => humanMove(index)}
+                            />
+                        );
+                    })
+                }
+            </Container>
         );
 
     }
@@ -54,5 +129,13 @@ const Inner = styled.div`
 `;
 
 const ChooseText = styled.p``;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  width: ${({ dims }) => `${dims * (SQUARE_DIMS + 5)}px`};
+  flex-flow: wrap;
+  position: relative;
+`;
 
 export default Tictactoe;
